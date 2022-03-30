e7cb14596e189b34a6f24f030740ab03b232e0c3
pc <pc@DESKTOP-H6MJR8K>
2022-03-30 20:38:27 +0300

    Add project files.

---
+++ AsyncStreams/MovieDataManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    internal sealed class MovieDataManager
+    {
+        private static readonly SqlClientFactory sqlClientFactory = SqlClientFactory.Instance;        
+
+        private static DbConnection CreateDbConnection()
+        {
+            var dbConnection = sqlClientFactory.CreateConnection();
+            dbConnection.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=MovieDb;Integrated Security=True;TrustServerCertificate=True;";
+            return dbConnection;
+        }
+
+        public async Task<IEnumerable<Movie>> GetAllMovies()
+        {
+            var dbConnection = CreateDbConnection();
+            DbCommand dbCommand = null;
+            DbDataReader dbDataReader = null;
+            try
+            {
+                await dbConnection.OpenAsync().ConfigureAwait(false);
+                dbCommand = CommandFactoryMovies.CreateCommandForGetAllMovies(dbConnection);
+                dbDataReader = await dbCommand.ExecuteReaderAsync().ConfigureAwait(false);
+                return await MapToMovies(dbDataReader);
+            }
+            finally
+            {
+                dbDataReader?.Dispose();
+                dbCommand?.Dispose();
+                dbConnection.Dispose();
+            }
+        }
+
+        private static async Task<IEnumerable<Movie>> MapToMovies(DbDataReader dbDataReader)
+        {
+            var movies = new List<Movie>();
+
+            while (await dbDataReader.ReadAsync())
+            {
+                movies.Add(new Movie(
+                    title: (string)dbDataReader[0],
+                    genre: GenreParser.Parse((string)dbDataReader[1]),
+                    year: (int)dbDataReader[2],
+                    imageUrl: (string)dbDataReader[3]));
+            }
+
+            return movies;
+        }
+
+        public async IAsyncEnumerable<Movie> GetAllMoviesAsyncStreaming()
+        {
+            var dbConnection = CreateDbConnection();
+            DbCommand dbCommand = null;
+            DbDataReader dbDataReader = null;
+            try
+            {
+                await dbConnection.OpenAsync().ConfigureAwait(false);
+                dbCommand = CommandFactoryMovies.CreateCommandForGetAllMovies(dbConnection);
+                dbDataReader = await dbCommand.ExecuteReaderAsync().ConfigureAwait(false);                
+
+                var movie = new Movie();
+                while (await dbDataReader.ReadAsync().ConfigureAwait(false))
+                {
+                    movie.Title = (string)dbDataReader[0];
+                    movie.Genre = GenreParser.Parse((string)dbDataReader[1]);
+                    movie.Year = (int)dbDataReader[2];
+                    movie.ImageUrl = (string)dbDataReader[3];
+                    yield return movie;
+                }
+            }
+            finally
+            {
+                dbDataReader?.Dispose();
+                dbCommand?.Dispose();
+                dbConnection.Dispose();
+            }
+        }
+
+        public async Task CreateMoviesTvpMergeInsertInto(IEnumerable<Movie> movies)
+        {
+            DbConnection dbConnection = CreateDbConnection();
+            DbTransaction dbTransaction = null;
+            DbCommand dbCommand = null;
+            try
+            {
+                await dbConnection.OpenAsync().ConfigureAwait(false);
+                dbTransaction = dbConnection.BeginTransaction();
+                dbCommand = CommandFactoryMovies.CreateCommandForCreateMoviesTvpMergeInsertInto(dbConnection, dbTransaction, movies);
+                await dbCommand.ExecuteNonQueryAsync();
+                dbTransaction.Commit();
+            }
+            catch (DbException)
+            {
+                dbTransaction?.Rollback();
+                throw;
+            }
+            finally
+            {
+                dbCommand?.Dispose();
+                dbTransaction?.Dispose();
+                dbConnection.Dispose();
+            }
+        }
+    }
+}
---
+++ AsyncStreams/Program.cs
@@ -0,0 +1,93 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{    
+    [MemoryDiagnoser]
+    public class Program
+    {
+        private static readonly int[] s_validYears = new int[] { 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 };
+        private static readonly string[] s_validGenres = GenreParser.GenreValues.ToArray();
+
+        private readonly MovieDataManager _movieDataManager;
+
+        static void Main()
+        {
+            /*
+             * Do this only the first time when you need
+             * to initilize the database with data.
+             * Also modify the Main() method's signature
+             * to be async and returning a Task instead of void
+             */
+            ////await InitializeDataInDatabase(10000);
+
+            BenchmarkRunner.Run<Program>();
+        }
+
+        public Program()
+        {
+            _movieDataManager = new MovieDataManager();
+        }
+
+        [Benchmark]
+        public async Task<int> GetAllMoviesAsyncStreaming()
+        {
+            var total = 0;
+
+            await foreach (var movie in _movieDataManager.GetAllMoviesAsyncStreaming().ConfigureAwait(false))
+            {
+                total += movie.Year;
+            }
+
+            return total;
+        }
+
+        [Benchmark]
+        public async Task<int> GetAllMovies()
+        {
+            var total = 0;
+            var allMovies = await _movieDataManager.GetAllMovies().ConfigureAwait(false);
+
+            foreach (var movie in allMovies)
+            {
+                total += movie.Year;
+            }
+            
+            return total;
+        }
+
+        private static async Task InitializeDataInDatabase(int numberOfRecords)
+        {
+            var movieDataManager = new MovieDataManager();
+            var uniqueMovieTitles = Randomizer.GenerateUniqueAsciiStrings(numberOfRecords);
+            var movies = GetRandomMovies(numberOfRecords, uniqueMovieTitles);
+            await movieDataManager.CreateMoviesTvpMergeInsertInto(movies);
+        }
+
+        private static IEnumerable<Movie> GetRandomMovies(int requiredCount, string[] uniqueMovieTitles)
+        {
+            var count = 0;
+            do
+            {
+                yield return CreateRandomMovie(uniqueMovieTitles[count]);
+                count++;
+            } while (count < requiredCount);
+        }
+
+        private static Movie CreateRandomMovie(string title)
+        {
+            var shuffledYears = Randomizer.ShuffleArray(s_validYears);
+            var shuffledGenres = Randomizer.ShuffleArray(s_validGenres);
+
+            return new Movie(
+                title: title,
+                genre: GenreParser.Parse(shuffledGenres[0]),
+                year: shuffledYears[0],
+                imageUrl: "http://www." + Randomizer.GetRandomAciiStringNoPunctuations(20) + ".com");
+        }
+    }
+}
